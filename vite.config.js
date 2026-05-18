fea50d9f89ccadccb54d29f577ea94f78306dea7
fakeMichael1234 <michaelsebastian399@gmail.com>
2026-05-18 19:17:11 +0530

    Fix Firebase login popup Cross-Origin policy & add env variables to build

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+      "Cross-Origin-Embedder-Policy": "unsafe-none",
+    },
+  },
+});
